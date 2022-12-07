27aa97911b80edc928d5d4e017f0577077af2301
babulalt <babulal.tamang@berrybytes.com>
2022-12-07 09:43:44 +0545

    profile tab implementation and other refactor

---
+++ src/components/Profile.js
@@ -0,0 +1,84 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { StudentGeneral } from './StudentGeneral';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+export const Profile=()=> {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box
+      sx={{ flexGrow: 1, bgcolor: 'background.paper', display: 'flex' }}
+    >
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        sx={{ borderRight: 1, borderColor: 'divider' }}
+      >
+        <Tab label="General Information" {...a11yProps(0)} />
+        <Tab label="Address Information" {...a11yProps(1)} />
+        <Tab label="Course Inforamtion" {...a11yProps(2)} />
+        <Tab label="Fee Information" {...a11yProps(3)} />
+        <Tab label="Education Inforamtion" {...a11yProps(4)} />
+      </Tabs>
+      <TabPanel value={value} index={0}>
+       <StudentGeneral/>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Address Information
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+       Course Inforamtion
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        Fee Inforamtion
+      </TabPanel>
+      <TabPanel value={value} index={4}>
+        Education Information
+      </TabPanel>
+    </Box>
+  );
+}
---
+++ src/components/StudentGeneral.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+
+
+
+export const StudentGeneral=()=> {
+  return (
+   <div>
+    <Typography>Student General Inforamtion</Typography>
+   </div>
+  );
+}
